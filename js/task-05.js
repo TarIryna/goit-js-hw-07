72f7c4fcb1af93d235000e8c856a661e4418ab3e
Tar Iryna <tar86irina@gmail.com>
2021-06-11 22:28:39 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,11 @@ const refs = {
     input: document.querySelector('[id="name-input"]'),
     nameLabel: document.querySelector('[id="name-output"]'),
 };
+
 refs.input.addEventListener('input', onInputChange);
 function onInputChange(event) {
-    refs.nameLabel.textContent = event.currentTarget.value;
+    if (refs.input.value.trim()) {
+        refs.nameLabel.textContent = event.currentTarget.value;
+    }
+    else {refs.nameLabel.textContent = "незнакомец";}
 }
